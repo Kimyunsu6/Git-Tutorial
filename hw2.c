b885a1181e537e2e53d6340a7de638d95e3b0240
Kimyunsu6 <202110916@sangmyung.kr>
2021-10-05 14:39:43 +0900

    3주차 과제

---
+++ hw2.c
@@ -0,0 +1,13 @@
+ 
+int main(void)
+{
+	int kilometers;
+	int miles;
+
+	printf("Please enker kilometers: \n");
+	scanf_s("%d", &kilometers);
+
+	miles = 1.609 * kilometers;
+
+	printf("%d km is equal to %.1f \n", kilometers, miles);
+}
