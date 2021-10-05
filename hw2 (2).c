145456d8d6d3db6ed230c46b2487087ba80cfef2
Kimyunsu6 <202110916@sangmyung.kr>
2021-10-05 18:09:45 +0900

    3주차 과제

---
+++ hw2 (2).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void) {
+
+	double mile, km;
+
+	printf("Please enter kilometers: ");
+
+	scanf_s("%lf", &km);
+
+	mile = km / 1.609;
+
+	printf("%.1f km is equal to %.1f miles.\n", km, mile);
+
+	return 0;
+
+}
